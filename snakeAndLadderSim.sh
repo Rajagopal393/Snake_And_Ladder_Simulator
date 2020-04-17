b630a59e823576a4ad59063bcd22ff24b1df7401
Raj Gopal <harirajagopal393@gmail.com>
2020-04-17 23:57:57 +0530

    Added feature to enable game for 2 players

--- snakeAndLadderSim.sh
+++ snakeAndLadderSim.sh
@@ -1,28 +1,68 @@
 #!/bin/bash 
-NUM_OF_PLAYERS=1
+NUM_OF_PLAYERS=2
 START_POSITION=0
-rollDice=$((RANDOM%6))
 currentPosition=0
 IS_LADDER=0
 IS_SNAKE=1
 IS_NO_PLAY=2
-positionDecider=$((RANDOM%3))
+WINNING_POSITION=100
+timesDiceRolled=0
+declare -A playerPosition
 
-ECHO "Dice rolled on $rollDice"
 
-case $positionDecider in 
-	$IS_LADDER )
-		$currentPosition=$(($currentPosition+$rollDice));
-		;;
-	$IS_SNAKE )
-		$currentPosition=$(($currentPosition-$rollDice));
-		;;
-	$IS_NO_PLAY )
-		echo "STAY STIL...!";
-		;;
-esac
+makingMove(){
 
-echo "current position : $currentPosition"
+        timesDiceRolled=$(($timesDiceRolled+1))
+        rollDice=$((RANDOM%6+1))
+        positionDecider=$((RANDOM%3))
+        case $positionDecider in
+            $IS_LADDER )
+                playerPosition[$player]=$((${playerPosition[$player]}+$rollDice))
+                if [ ${playerPosition[$player]} -gt $WINNING_POSITION ]
+                then
+			playerPosition[$player]=$((${playerPosition[$player]}+0))
+                    else
+                        playerPosition[$player]=$((${playerPosition[$player]}+$rollDice));
+                fi
+                ;;
+            $IS_SNAKE )
+                playerPosition[$player]=$((${playerPosition[$player]}-$rollDice))
+                if [ ${playerPosition[$player]} -lt $START_POSITION ]
+                    then
+                        playerPosition[$player]=$START_POSITION
+                else
+                        playerPosition[$player]=$((${playerPosition[$player]}-$rollDice));
+                fi
+                ;;
+            $IS_NO_PLAY )
+                playerPosition[$player]=$((${playerPosition[$player]}+0))
+                ;;
+        esac
+    
+}
+gameStatus=0
+gameOver=0
+findingWinner(){
+	
+	while [ $gameStatus -eq $gameOver ]
+	do
+		for (( player=1; player<=$NUM_OF_PLAYERS; player++ ))
+			do
+				makingMove
+				if [ ${playerPosition[$player]} -eq $WINNING_POSITION ]
+				then
+					winner=$player
+					gameOver=1
+					break;
+			fi
+		done
+	done
+}
+
+findingWinner
+
+echo "Winner is Player number : $winner"
+
 	
 	
 		
