df700756995e0b8bc208eb11fd0a9e0c52fac43a
Raj Gopal <harirajagopal393@gmail.com>
2020-04-17 18:02:24 +0530

    Added feature to decide next step of the player based on rolled number

--- snakeAndLadderSim.sh
+++ snakeAndLadderSim.sh
@@ -1,6 +1,28 @@
 #!/bin/bash 
-noOfPlayers=1
-startPosition=0
+NUM_OF_PLAYERS=1
+START_POSITION=0
 rollDice=$((RANDOM%6))
+currentPosition=0
+IS_LADDER=0
+IS_SNAKE=1
+IS_NO_PLAY=2
+positionDecider=$((RANDOM%3))
 
 ECHO "Dice rolled on $rollDice"
+
+case $positionDecider in 
+	$IS_LADDER )
+		$currentPosition=$(($currentPosition+$rollDice));
+		;;
+	$IS_SNAKE )
+		$currentPosition=$(($currentPosition-$rollDice));
+		;;
+	$IS_NO_PLAY )
+		echo "STAY STIL...!";
+		;;
+esac
+
+echo "current position : $currentPosition"
+	
+	
+		
